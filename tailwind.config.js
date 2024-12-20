b9dfdbdd74ee936f839935a358f9f526f29032cf
Sam Daramroei <samdotmc@gmail.com>
2024-12-20 23:19:11 +0300

    add Tailwind CSS setup and initial any-promise typings

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./*.html", "./js/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#003366",
+        secondary: "#F4C430",
+        accent: "#66BB6A",
+        background: "#F8F4E3",
+      },
+    },
+  },
+  plugins: [],
+};
