6cde5d404ec2b57ec7834ee8d7e761d0447ed21c
Sam Daramroei <samdotmc@gmail.com>
2025-03-02 15:07:14 +0300

    fix: update hamburger button functionality and improve mobile menu toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,9 @@
 // Wait for the DOM to be fully loaded before running the script
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.getElementById("hamburger");
-  const navMenu = document.getElementById("nav-menu");
+  const mobileMenu = document.getElementById("mobile-menu-2");
 
   hamburger.addEventListener("click", () => {
-    navMenu.classList.toggle("hidden");
-  });
-  const menuToggle = document.getElementById("menu-toggle");
-  menuToggle.addEventListener("click", () => {
-    const menu = document.getElementById("mobile-menu");
-    menu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("hidden");
   });
 });
